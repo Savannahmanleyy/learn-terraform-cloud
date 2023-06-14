e7a3bd7e8525efd31d7511bad80abff4040cfd92
Savannah Manley <savannma@umich.edu>
2023-06-14 09:31:19 -0500

    Remove cloud block

---
+++ terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+/*
+  cloud {
+    organization = "hashi-vmans"
+
+    workspaces {
+      name = "learn-terraform-cloud"
+    }
+  }
+*/
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.3.0"
+    }
+  }
+
+  required_version = ">= 0.14.0"
+}
